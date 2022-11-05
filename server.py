032b29715133e8cea18c6e286929c9ed7af9c8ed
Nuruddin <Nuruddin6ix@gmail.com>
2022-11-05 20:17:08 +0100

    Final Commit

---
+++ server.py
@@ -0,0 +1,57 @@
+from concurrent.futures import thread
+from email import message
+import threading
+import socket
+
+
+host_ip = '127.0.0.1' # test with localhost
+port = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host_ip, port))
+server.listen()
+
+# set client list/names
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat room!'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'Connected with {str(address)}')
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of the client is {nickname}!')
+        broadcast(f'{nickname} joined the group chat!'.encode('ascii'))
+        client.send('Connected to the server!'.encode('ascii'))
+
+        # X11Y3R - CHANGE TO MULTITHREAD
+        # ------
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print('Server X11Y3R is listening...')
+receive()
